7a0f0fc89958ccd4206fe44b3c95832ae198d5d4
PongPong <liuhsin-yi@PongPongde-MacBook-Pro.local>
2019-08-26 03:06:40 +0800

    change img src

--- src/app/projects/layout-practice/layout-practice.component.ts
+++ src/app/projects/layout-practice/layout-practice.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutPracticeComponent implements OnInit {
 
-  public readonly imgSrc = '../../../assets/attractionPicture.jpg';
+  public readonly imgSrc = './assets/attractionPicture.jpg';
   public readonly title = '切版練習標題切版練習標題切版練習標題切版練習標題切版練習標題切版練習標題';
   public readonly content = '切版練習內容切版練習內容切版練習內容切版練習內容切版練習內容切版練習內容切版練習內容切版練習內容切版練習內容切版練習內容';
   constructor() { }
